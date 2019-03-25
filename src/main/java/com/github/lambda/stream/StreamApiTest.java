38ecb3b11b82ecc30fee1c992ae512c17978c903
chenxingfei <chenxingfei@mintechai.com>
2019-03-25 22:29:36 +0800

    增加java8的Stream对象的简单用法

--- src/main/java/com/github/lambda/stream/StreamApiTest.java
+++ src/main/java/com/github/lambda/stream/StreamApiTest.java
@@ -1,10 +1,13 @@
 package com.github.lambda.stream;
 
 import com.github.lambda.model.User;
+import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * @author chenxingfei
@@ -28,8 +31,6 @@ public class StreamApiTest {
 
 
 
-
-
     /**
      * 筛选和切片
      * filter:按照条件进行过滤，返回一个满足条件的新流
@@ -45,5 +46,55 @@ public class StreamApiTest {
                 .distinct()
                 .forEach(System.out::println);
 
+    }
+
+    /**
+     * map 映射
+     * map方法将元素转换器为其他形式或提取信息，接收一个函数作为参数，该函数会被应用到每个元素上，并将其映射成一个新的元素。
+     * flagMap接收一个函数作为参数，将流中每个值都换成另一个流，然后把所有流连成一个流
+     */
+    @Test
+    public void testMap(){
+
+        List<String> collect = users.get().stream().map(User::getUserName).collect(Collectors.toList());
+        List<List<Character>> collect1 = collect.stream().map(StreamApiTest::filterCharacter).collect(Collectors.toList());
+        System.out.println(collect1);
+
+        // flatMap接收一个函数作为参数，将流中每个值都换成另一个流，然后把所有流连成一个流
+        Stream<Character> rStream = collect.stream().flatMap(StreamApiTest::filterCharacterByStream);
+        rStream.forEach(System.out::println);
+    }
+
+    /**
+     * 过滤字符串
+     * @param chars
+     * @return
+     */
+    public static List<Character> filterCharacter(String chars){
+
+        List<Character> character = Lists.newArrayList();
+
+
+        for (char c : chars.toCharArray()) {
+            character.add(c);
+        }
+        return character;
+    }
+
+
+    /**
+     * 过滤字符串
+     * @param chars
+     * @return
+     */
+    public static Stream<Character> filterCharacterByStream(String chars){
+
+        List<Character> character = Lists.newArrayList();
+
+
+        for (char c : chars.toCharArray()) {
+            character.add(c);
+        }
+        return character.stream();
     }
 }
