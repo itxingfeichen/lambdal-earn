5238d40467cfef0fad4f15c999d0051b754f1e27
chenxingfei <chenxingfei@mintechai.com>
2019-03-25 23:10:39 +0800

    增加java8的Stream对象的简单用法

--- src/main/java/com/github/lambda/stream/StreamApiTest.java
+++ src/main/java/com/github/lambda/stream/StreamApiTest.java
@@ -5,7 +5,9 @@ import com.google.common.collect.Lists;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -22,13 +24,12 @@ public class StreamApiTest {
             new User("李四", 21, "李四@github.com"),
             new User("王五", 12, "王五@github.com"),
             new User("赵六", 32, "赵六@github.com"),
-            new User("赵六", 32, "赵六@github.com"),
-            new User("赵六", 32, "赵六@github.com"),
+            new User("赵六", 22, "赵六@github.com"),
+            new User("赵六", 100, "赵六@github.com"),
             new User("周五", 33, "周五@github.com"),
             new User("正王", 43, "正王@github.com")
 
     ));
-
 
 
     /**
@@ -39,8 +40,8 @@ public class StreamApiTest {
      * distinct:去重复（需要重新hashCode和equals）
      */
     @Test
-    public void testSplit(){
-        users.get().stream().filter(o->o.getAge()>1)
+    public void testSplit() {
+        users.get().stream().filter(o -> o.getAge() > 1)
                 .limit(5)
                 .skip(1)
                 .distinct()
@@ -54,7 +55,7 @@ public class StreamApiTest {
      * flagMap接收一个函数作为参数，将流中每个值都换成另一个流，然后把所有流连成一个流
      */
     @Test
-    public void testMap(){
+    public void testMap() {
 
         List<String> collect = users.get().stream().map(User::getUserName).collect(Collectors.toList());
         List<List<Character>> collect1 = collect.stream().map(StreamApiTest::filterCharacter).collect(Collectors.toList());
@@ -65,18 +66,51 @@ public class StreamApiTest {
         rStream.forEach(System.out::println);
     }
 
+    /**
+     * 查找和匹配
+     * allMatch(),anyMatch(),noneMatch(),findFirst(),findAny(),count(),max(),min()
+     */
     @Test
-    public void testSorted(){
-        List<String> sorts = Arrays.asList("222","888","444","333","111","000");
+    public void testSearchAndMatch() {
+        // 匹配所有
+        boolean b = users.get().stream().allMatch(action -> action.getAge() < 1);
+        System.out.println(b);
+        // 任意匹配
+        boolean b1 = users.get().stream().anyMatch(action -> action.getAge() > 1);
+        System.out.println(b1);
+        // 校验是否含某个条件的数据，若含有返回false
+        boolean b2 = users.get().stream().noneMatch(action -> action.getUserName().equals("张三xx"));
+        System.out.println(b2);
+        // 查找满足条件的第一个元素
+        Optional<User> first = users.get().stream().filter(user -> user.getAge() > 32).findFirst();
+        System.out.println(first.get());
+        // 查找满足条件的任意一个元素
+        Optional<User> result = users.get().parallelStream().filter(user -> user.getUserName().equals("赵六")).findAny();
+        System.out.println(result.get());
+        // 统计个数
+        System.out.println(users.get().stream().count());
+        // 查询满足条件的最大值
+        Optional<User> max = users.get().stream().max((Comparator.comparing(User::getAge)));
+        System.out.println(max.get());
+        // 获取最小年龄并返回年龄
+        Optional<Integer> min = users.get().stream().map(User::getAge).min(Integer::compareTo);
+        System.out.println(min.get());
+
+
+    }
+
+    @Test
+    public void testSorted() {
+        List<String> sorts = Arrays.asList("222", "888", "444", "333", "111", "000");
         // 自然排序
         sorts.stream().sorted().forEach(System.out::println);
 
         // 定制排序
-        users.get().stream().sorted((x,y)->{
-            if(x.getAge().compareTo(y.getAge())==0){
-              return  x.getUserName().compareTo(y.getUserName());
-            }else{
-              return  x.getAge().compareTo(y.getAge());
+        users.get().stream().sorted((x, y) -> {
+            if (x.getAge().compareTo(y.getAge()) == 0) {
+                return x.getUserName().compareTo(y.getUserName());
+            } else {
+                return x.getAge().compareTo(y.getAge());
             }
         }).forEach(System.out::println);
 
@@ -84,10 +118,11 @@ public class StreamApiTest {
 
     /**
      * 过滤字符串
+     *
      * @param chars
      * @return
      */
-    public static List<Character> filterCharacter(String chars){
+    public static List<Character> filterCharacter(String chars) {
 
         List<Character> character = Lists.newArrayList();
         for (char c : chars.toCharArray()) {
@@ -99,10 +134,11 @@ public class StreamApiTest {
 
     /**
      * 过滤字符串
+     *
      * @param chars
      * @return
      */
-    public static Stream<Character> filterCharacterByStream(String chars){
+    public static Stream<Character> filterCharacterByStream(String chars) {
 
         List<Character> character = Lists.newArrayList();
 
